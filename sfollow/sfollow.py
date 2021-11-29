950afbccc426e2a16a2c001fb4f3ca1655525ff0
Thomas Kluyver <thomas@kluyver.me.uk>
2021-11-29 17:37:18 +0000

    Rework again, back to plain functions with no async

--- sfollow/sfollow.py
+++ sfollow/sfollow.py
@@ -1,11 +1,10 @@
-import codecs
+import itertools
 import os
 import re
 import sys
+import time
 from collections import defaultdict
 from subprocess import run, PIPE
-
-import trio
 
 from .terminal import msg, spinner_msg, clear_spinner, fmt_state, fmt_jobs
 
@@ -24,73 +23,62 @@ def job_states(job_ids):
     return dict([l.strip().partition(' ')[::2] for l in res.stdout.splitlines()])
 
 
-async def watch_jobs(job_ids, nursery):
+def sfollow(job_ids):
     states = job_states(job_ids)
-    tail_cancels = defaultdict(list)
+    open_files = defaultdict(list)
 
+    # Jobs already running before we started: jump to near the end, like tail -f
     for job_id, state in states.items():
         if state not in STATES_NOT_STARTED:
             info = get_job_info(job_id)
             for path in get_std_streams(info):
-                cscope = await nursery.start(tail_log, path)
-                tail_cancels[job_id].append(cscope)
+                fh = open(path, 'rb')
+                if os.stat(fh.fileno()).st_size > 512:
+                    fh.seek(-512, os.SEEK_END)
+                open_files[job_id].append(fh)
 
-    while True:
-        job_ids_unfinished = [
-            j for (j, s) in states.items() if s not in STATES_FINISHED
-        ]
+    for i in itertools.count():
+        for files in open_files.values():
+            for f in files:
+                for line in f:
+                    print(line.decode('utf-8', 'replace'), end='')
 
-        if all(st in STATES_NOT_STARTED for st in states.values()):
-            spinner_msg(f"Waiting for {fmt_jobs(job_ids)} to start")
+        # Check for jobs started/finished every 4th cycle (2 seconds)
+        if i % 4 == 0:
+            if not open_files:
+                spinner_msg(f"Waiting for {fmt_jobs(job_ids)} to start")
 
-        for job_id, new_state in job_states(job_ids_unfinished).items():
+            new_states = job_states([
+                j for (j, s) in states.items() if s not in STATES_FINISHED
+            ])
+        else:
+            new_states = {}
+
+        for job_id, new_state in new_states.items():
             started = new_state not in STATES_NOT_STARTED
             if started and states[job_id] in STATES_NOT_STARTED:
                 # Job started since the last check
                 info = get_job_info(job_id)
                 clear_spinner()
                 msg(f"Job {job_id} ({info.get('JobName', '')}) started")
-                for i, path in enumerate(get_std_streams(info)):
-                    cscope = await nursery.start(tail_log, path, True)
-                    tail_cancels[job_id].append(cscope)
+                for path in get_std_streams(info):
+                    open_files[job_id].append(open(path, 'rb'))
 
             if new_state in STATES_FINISHED:
                 # Job finished since the last check
-                for cscope in tail_cancels.pop(job_id):
-                    cscope.cancel()
+                for fh in open_files.pop(job_id, ()):
+                    # strip any trailing newline, let print() add one.
+                    print(fh.read().replace('utf-8', 'replace').rstrip('\n'))
+                    fh.close()
 
-                # Fudge - hopefully final output will show before 'finished' msg
-                await trio.sleep(0.01)
                 msg(f'Job {job_id} finished ({fmt_state(new_state)})')
 
             states[job_id] = new_state
 
         if all(st in STATES_FINISHED for st in states.values()):
-            break
+            break  # All jobs finished, sfollow can exit
 
-        await trio.sleep(2)
-
-
-async def tail_log(path, newly_started=False, *, task_status=trio.TASK_STATUS_IGNORED):
-    with open(path, 'rb') as fh:
-        if not newly_started and os.stat(fh.fileno()).st_size > 512:
-            fh.seek(-512, os.SEEK_END)
-
-        sr = codecs.getreader('utf-8')(fh, 'replace')
-
-        with trio.CancelScope() as cscope:
-            task_status.started(cscope)
-
-            while True:
-                for line in sr:
-                    print(line, end='')
-                await trio.sleep(0.5)
-
-        # The Slurm job has finished - this should catch any final output
-        # Not unreachable, see https://youtrack.jetbrains.com/issue/PY-34484
-        # noinspection PyUnreachableCode
-        for line in sr:
-            print(line, end='')
+        time.sleep(0.5)
 
 
 def get_job_info(job_id):
@@ -120,14 +108,6 @@ def get_std_streams(job_info):
     return paths
 
 
-async def sfollow(job_ids):
-    """Follow the output from a SLURM batch job"""
-    async with trio.open_nursery() as nursery:
-        nursery.start_soon(watch_jobs, job_ids, nursery)
-    # The nursery waits for all child tasks to finish, which should happen
-    # shortly after the Slurm jobs in question finish.
-
-
 def my_last_job():
     # '--format=%i %j' gives job IDs & names
     # --sort=-V sorts by submission time (descending)
@@ -146,7 +126,7 @@ def main():
         job_id, job_name = my_last_job()
         job_ids = [job_id]
         msg(f"Following your most recent job: {job_id} ({job_name})")
-    trio.run(sfollow, job_ids)
+    sfollow(job_ids)
 
 if __name__ == '__main__':
     main()
